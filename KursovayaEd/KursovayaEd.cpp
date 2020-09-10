44a7d5e1868a1b32d8d33020a58f057ec69dca54
Dikobrazzzec <zasxcd1307@mail.ru>
2020-09-10 14:22:35 +0300

    working inout+-/

---
+++ KursovayaEd/KursovayaEd.cpp
@@ -0,0 +1,76 @@
+﻿#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+class LongNum {
+public:
+	long valueNum;
+
+	LongNum() {}
+
+	long input() {
+		cout << "Enter value" << endl;
+		cin >> valueNum;
+		return valueNum;
+	}
+
+	long output() {
+		cout << "Your value is " << valueNum << endl;
+		return valueNum;
+	}
+
+	LongNum operator+(LongNum secondTerm) {
+		LongNum result;
+		result.valueNum = valueNum + secondTerm.valueNum;
+		return result;
+	}
+
+	LongNum operator-(LongNum subtrahend) {
+		LongNum result;
+		result.valueNum = valueNum - subtrahend.valueNum;
+		return result;
+	}
+
+	LongNum operator/(LongNum divider) {
+		LongNum result;
+		result.valueNum = valueNum / divider.valueNum;
+		return result;
+	}
+
+	long Sq() {
+		valueNum = sqrt(valueNum);
+		return valueNum;
+	}
+};
+
+int main() {
+	LongNum A;
+	LongNum B;
+	B.valueNum = 5;
+	A.input();
+	cout << "**********************************************************" << endl;
+	cout << "**********************************************************" << endl;
+	cout << "**********************************************************" << endl;
+	A.output();
+	cout << "**********************************************************" << endl;
+	cout << "**********************************************************" << endl;
+	cout << "**********************************************************" << endl;
+	A = A + B;
+	A.output();
+	cout << "**********************************************************" << endl;
+	cout << "**********************************************************" << endl;
+	cout << "**********************************************************" << endl;
+	A = A - B;
+	A.output();
+	cout << "**********************************************************" << endl;
+	cout << "**********************************************************" << endl;
+	cout << "**********************************************************" << endl;
+	A = A / B;
+	A.output();
+	cout << "**********************************************************" << endl;
+	cout << "**********************************************************" << endl;
+	cout << "**********************************************************" << endl;
+	A.Sq();
+	A.output();
+}
